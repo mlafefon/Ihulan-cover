955e20fd82bd0afd2ed28c3b9832c36a055d43bd
mlafefon <51256464+mlafefon@users.noreply.github.com>
2025-10-11 17:04:29 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, '.', '');    
     return {
+      base: '/Story-Designer-Landing-Page/',
       server: {
         port: 3000,
         host: '0.0.0.0',
