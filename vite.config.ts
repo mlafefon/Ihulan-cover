4464061342d9acb2045e5e6803643d51db68a885
mlafefon <51256464+mlafefon@users.noreply.github.com>
2025-10-11 17:13:06 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');    
     return {
-      base: '/Story-Designer-Landing-Page/',
+      base: '/Ihulan-cover/',
       server: {
         port: 3000,
         host: '0.0.0.0',
